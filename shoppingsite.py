55e4fe75e13dc2ff826075214dda6b891d2a43d3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-06 17:50:39 -0700

    Making Progress

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,7 +7,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,8 +50,8 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
-    print melon
+    melon = melons.get_by_id(melon_id)
+    # print melon
     return render_template("melon_details.html",
                            display_melon=melon)
 
@@ -78,6 +78,7 @@ def show_shopping_cart():
     # Make sure your function can also handle the case wherein no cart has
     # been added to the session
 
+
     return render_template("cart.html")
 
 
@@ -100,7 +101,18 @@ def add_to_cart(melon_id):
     # - flash a success message
     # - redirect the user to the cart page
 
-    return "Oops! This needs to be implemented!"
+
+    if "cart" not in session:
+        session["cart"] = {}
+
+    if melon_id not in session["cart"]:
+        session["cart"][melon_id] = 1
+        #session[melon_id] = count
+    else:
+        session["cart"][melon_id] += 1
+
+    flash("Sucessfully added melon to cart")
+    return redirect("/cart")
 
 
 @app.route("/login", methods=["GET"])
